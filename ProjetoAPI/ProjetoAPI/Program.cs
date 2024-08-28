e3c88eb7a2e30ca9cccab158fe05248bafd461c9
GuiKre <guilhermekre@gmail.com>
2024-08-28 11:28:21 -0300

    Funcionalidades

--- ProjetoAPI/ProjetoAPI/Program.cs
+++ ProjetoAPI/ProjetoAPI/Program.cs
@@ -1,6 +1,30 @@
+//Testar as APIs
+// - Rest Client - Extensão do VS Code
+// - Postman
+// - Insomnia
+//MINIMAL APIs
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//EndPoints - Funcionalidades
+//Configurar a URL e o método/verbo
+app.MapGet("/", () => "Primeira funcionalidade");
+
+app.MapGet("/segundafuncionalidade", () => "Segunda funcionalidade");
+
+app.MapGet("/retornarendereco", () =>
+{
+    dynamic endereco = new {
+        rua = "Parigot de Souza",
+        numero = 5300,
+        cep = "81290-350"
+    };
+    return endereco;
+});
+
+//Criar uma funcionalidade para receber informações
+// - Receber informações pela URL da requisição
+// - Receber informações pelo corpo da requisição
+// - Guardar as informações em uma lista
 
 app.Run();
