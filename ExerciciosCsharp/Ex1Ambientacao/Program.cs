ff4c47c86ec20cae1c228b574eb71708080be8af
GuiKre <guilhermekre@gmail.com>
2024-09-04 08:42:46 -0300

    Att

---
+++ ExerciciosCsharp/Ex1Ambientacao/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Digite a altura do retângulo: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite a largura do retângulo: ");
+double largura = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("A área do retângulo é: " + altura * largura);
